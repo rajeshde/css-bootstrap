b02da6802eb7587988d3e1d0dd77650d4b4df87a
rajeshd <rajesh.de@onometra.com>
2021-06-18 09:06:33 +0530

    added cyan, lightBlue and custom spacing in tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightBlue: colors.lightBlue,
+        cyan: colors.cyan,
+      },
+      spacing: {
+        18: "4.5rem",
+      },
+    },
   },
   variants: {
     extend: {},
